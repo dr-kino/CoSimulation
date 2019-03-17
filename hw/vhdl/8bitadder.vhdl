a546093ab20179ac92a61cd05defeb83e1fc087d
dr-kino <doctorkinoo@gmail.com>
2019-03-17 13:41:08 +0000

    Modified Readme.md to presents ASCII art.

--- hw/vhdl/8bitadder.vhdl
+++ hw/vhdl/8bitadder.vhdl
@@ -1,10 +1,10 @@
--- File 8bitadder.vhd
+-- File bitadder.vhd
 -- Library
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 -- Definitions
-entity 8bitadder is
+entity bitadder is
 	port(
 		A		: in  std_logic_vector(7 downto 0); 
 		B		: in  std_logic_vector(7 downto 0);
@@ -13,9 +13,9 @@ entity 8bitadder is
 		C_OUT	: out std_logic;
 		OVERFLOW: out std_logic
 	);
-end 8bitadder;
+end bitadder;
 
-architecture behavior of 8bitadder is
+architecture behavior of bitadder is
 
 -- Component
 component full_adder
@@ -34,12 +34,17 @@ signal CARRY_OUT : std_logic_vector(8 downto 0);
 begin
 CARRY_OUT(0) <= C_IN;
 
-adderGen : for i in 0 to 7 generate
-	UADD: full_adder port map(
-		a => A(i) , b => B(i), cin => C_IN(i), so => S_O(i), cout => CARRY_OUT(i+1)
+uGen1 : for i in 0 to 7 generate
+	UFA0: full_adder port map(
+		a => A(i), 
+		b => B(i), 
+		cin => CARRY_OUT(i), 
+		so => S_O(i), 
+		cout => CARRY_OUT(i+1)
 	);	   
+end generate uGen1;
 
-	C_OUT <= CARRY_OUT(8);
-	OVERFLOW <= CARRY_OUT(7) xor CARRY_OUT(8);
+C_OUT <= CARRY_OUT(8);
+OVERFLOW <= CARRY_OUT(7) xor CARRY_OUT(8);
 
 end behavior;
