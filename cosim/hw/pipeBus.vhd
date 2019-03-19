0ff7a0766fff20db508bd0f0781b16b3b7980a18
dr-kino <doctorkinoo@gmail.com>
2019-03-19 23:57:06 +0000

    Implemented entities for sink and get data from pipe interface.

---
+++ cosim/hw/pipeBus.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library std;
+use std.textio.all;
+
+entity pipe_bus is
+	generic
+	(
+		DataWidth_g : natural := 8;
+		InputFile_g : string := "sw2hw.txt"
+		OutputFile_g : string := "hw2sw.txt"
+	);
+	port
+	(
+		Ready_i : in std_logic;
+		DataA_i : in std_logic_vector(DataWidth_g - 1 downto 0);
+		DataB_i : in std_logic_vector(DataWidth_g - 1 downto 0);
+		Ready_o : out std_logic;
+		Data_o : out std_logic_vector(DataWidth_g - 1 downto 0)
+	);
+end entity pipe_bus;
+
+architecture behavioral of pipe_bus is
+
+begin
+
+end architecture behavioral;
---
+++ cosim/hw/pipeSink.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library std;
+use std.textio.all;
+
+entity pipe_sink is
+	generic
+	(
+		DataWidth_g : natural := 8;
+		OutFile_g : string := "sink.txt";
+		Signal_g : boolean := false 
+	);
+	port
+	(
+		Data_i : in std_logic_vector(DataWidth_g - 1 downto 0)
+	);
+end entity pipe_sink;
+
+architecture behavioral of pipe_sink is
+
+begin
+
+end architecture behavioral;
